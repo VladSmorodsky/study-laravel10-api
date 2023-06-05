c1785c201ddf543e2f731cadcbda0f21411e3620
Vlad Smorodsky <smorodsky.odesa@gmail.com>
2023-06-05 09:36:27 +0300

    ULRA-5 Project nested resources: added attaching members to project functionality

--- src/app/Http/Controllers/MemberController.php
+++ src/app/Http/Controllers/MemberController.php
@@ -12,4 +12,15 @@ class MemberController extends Controller
     {
         return new UserCollection($project->members);
     }
+
+    public function store(Request $request, Project $project)
+    {
+        $request->validate([
+            'user_id' => 'required|exists:users,id'
+        ]);
+
+        $project->members()->syncWithoutDetaching([$request->user_id]);
+
+        return new UserCollection($project->members);
+    }
 }
